64234162761bbd80c35afd11f9905b3681e6a6c5
mjchiquiza-glitch <mjchiquiza@gmail.com>
2026-03-24 21:55:00 -0500

    Create codigoint.c

---
+++ corte1/c/practica1/codigoint.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+   int main() {
+      int n1, n2, suma;
+   printf("Ingrese el primer digito: ");
+   scanf("%d", &n1);
+
+   printf("Ingrese el segundo digito: ");
+   scanf("&d", &n2); 
+      suma = n1 + n2;
+   printf("La suma de %d y %d es: %d\n", n1, n2, suma);
+return 0;
+}
