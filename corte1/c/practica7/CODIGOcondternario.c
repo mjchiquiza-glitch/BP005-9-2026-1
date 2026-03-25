50b121213246bcd5243d154646ca73611b819123
mjchiquiza-glitch <mjchiquiza@gmail.com>
2026-03-24 22:43:27 -0500

    Create CODIGOcondternario.c

---
+++ corte1/c/practica7/CODIGOcondternario.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+   int main() {
+     int hora;
+   printf("Ingrese la hora en militar: ");
+   scanf("%d", &hora);
+     (hora < 18) ? printf("Buenos dias.") : printf("Buenas noches.");
+return 0;
+}
