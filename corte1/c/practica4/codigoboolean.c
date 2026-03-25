77e58d8326725ef813de5bdb56ef25628b2affb8
mjchiquiza-glitch <mjchiquiza@gmail.com>
2026-03-24 22:24:57 -0500

    Create codigoboolean.c

---
+++ corte1/c/practica4/codigoboolean.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+   int main() {
+     int x= 25;
+     int y= 15;
+   bool isGreater = x > y;
+if (isGreater) {
+   printf("X is greater than y\n");
+}
+return 0;
+}
