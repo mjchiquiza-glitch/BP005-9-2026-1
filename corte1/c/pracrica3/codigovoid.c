d7e584f87c1badfdaa5d36a8a7afeda34b5a6b8a
mjchiquiza-glitch <mjchiquiza@gmail.com>
2026-03-24 22:15:00 -0500

    Create codigovoid.c

---
+++ corte1/c/pracrica3/codigovoid.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+   int sum(int x1, int x2) {
+     int result; 
+     result= x1 + x2;
+return result;
+}
+   void imprimir() {
+   printf("Hola mundo\n");
+}
+   int res(int x1, int x2) {
+     int result;
+     result = x1 - x2;
+return result;
+}
+   int mult(int x1, int x2) {
+     int result;
+     result= x1 * x2;
+return result;
+}
+   int main(){
+     int a, b, r;
+     a= 3;
+     b= 5;
+     r= sum(a,b);
+   printf("El resultado de la suma es: %d\n", r);
+   printf("El resultado de la resta es: %d\n", res(a,b));
+   printf("El resultado de la multiplicacion es: %d\n", mult(a,b));
+imprimir ();
+}
