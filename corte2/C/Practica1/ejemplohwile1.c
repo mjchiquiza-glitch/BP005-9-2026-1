f4ca7ba1d0da7861521454d936aa3a43663e8b3a
mjchiquiza-glitch <mjchiquiza@gmail.com>
2026-04-08 13:37:17 -0500

    Create ejemplohwile1.c

---
+++ corte2/C/Practica1/ejemplohwile1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int i= 1;//variabe de control
+
+  while (i <= 5) { //mientras i sea menor o igual que 5
+    printf ("%d\n", i); //muestra el valor actual
+    i++; //actualiza la variable
+  }
+return 0;
+}
