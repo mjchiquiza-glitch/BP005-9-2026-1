627b836f270c10ff8592801c7e5836746ed16d91
mjchiquiza-glitch <mjchiquiza@gmail.com>
2026-04-08 13:49:18 -0500

    Create ejemplowhile2.c

---
+++ corte2/C/Practica1/ejemplowhile2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+  int main() {
+    int contador = 1; //controla cauntas veces repetimos
+    int numero; //almacena cada numero ingresado
+    itn suma = 0; //acumulador
+ while ()
