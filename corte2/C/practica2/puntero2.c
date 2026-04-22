4a60c39bda7bce687da7c77b249ccbb122e0c606
mjchiquiza-glitch <mjchiquiza@gmail.com>
2026-04-22 13:18:05 -0500

    Create puntero2.c

---
+++ corte2/C/practica2/puntero2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+  void intercambiar(int *a, int *b) {
+    int temp = *a;
+    *a = *n;
+    *b = temp;
+}
+  int main() {
+    int x = 10;
+    int y = 20;
+
+prinft("Antes; x = %d, y = %d\n", x, y);
+intercabiar(&x, &y);
+printf("Despues; x %d, y %d\n", x, y);
+  return 0;
+}
