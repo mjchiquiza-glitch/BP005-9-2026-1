3f270781d69fff5cc6bff5291dd2f1845eb90d66
mjchiquiza-glitch <mjchiquiza@gmail.com>
2026-06-04 23:31:57 -0500

    Create tableroh.cpp

---
+++ corte2/juegoproyectofinal/tablero/tableroh.cpp
@@ -0,0 +1,12 @@
+#ifndef TABLERO_H
+#define TABLERO_H
+
+// Definimos el tamaño del tablero
+const int FILAS = 15;
+const int COLUMNAS = 20;
+
+// Funciones obligatorias para el tablero
+void limpiarTablero(char tablero[FILAS][COLUMNAS]);
+void mostrarTablero(char tablero[FILAS][COLUMNAS]);
+
+#endif
